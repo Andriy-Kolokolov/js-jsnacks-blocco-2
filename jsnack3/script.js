b8cd70acaad192cbb40009904ca4be86369e7888
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-20 12:51:17 +0200

    jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,9 +1,17 @@
-let arrayNames = ["Stefano", "Vincenzo", "Paolo", "Andrea", "Irene", "Henri", "Gianluca", "Alessio", "Davide"];
-let arrayLastNames =["Minardi", "Rossi", "Spinelli", "Caiazzo", "Ferrari", "Lazzari", "Falco", "Kapidani", "Falcone"];
+let arrayNums = [];
 
-let arrayFakeNames = [];
-
-for (let i = 0; i < arrayNames.length; i++) {
-    arrayFakeNames[i] = arrayNames[i] + " " + arrayLastNames[i];
+for (let i = 0; i < 100; i++) {
+    arrayNums.push(Math.floor(Math.random() * 10))
 }
-console.log(arrayFakeNames)
+
+let oddNumsSum = 0;
+
+for (let i = 0; i < arrayNums.length; i++) {
+    if (i % 2 === 0) {
+        oddNumsSum += arrayNums.at(i);
+    }
+}
+
+console.log(arrayNums);
+
+console.log("SUM OF ODD INDEX NUMS => " + oddNumsSum);
